bdfc3af22a0855aba947255765ba29e5808a7721
nabixka <nabixka05@gmail.com>
2026-06-19 11:17:58 +0700

    Type Kits

--- backend/src/kits_type_game/kits_type_game.controller.ts
+++ backend/src/kits_type_game/kits_type_game.controller.ts
@@ -1,6 +1,7 @@
-import { Controller, Delete, Get, Param, Post } from '@nestjs/common';
+import { BadRequestException, Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
 import { KitsTypeGameService } from './kits_type_game.service';
 import { ValidateGameExist } from 'src/Pipes/ValidateGameExist';
+import { ValidateKitsTypeExist } from 'src/Pipes/ValidateKitsTypeExist';
 
 @Controller('kits-type')
 export class KitsTypeGameController {
@@ -16,9 +17,19 @@ export class KitsTypeGameController {
     return this.kitsTypeGameService.getByGame(Number(game_id))
   }
 
-  @Post()
-  createKits(){
+  @Post('/game/:game_id')
+  createKits(@Param('game_id', ValidateGameExist) game_id: string, @Body() data: { kits_type_name: string }){
+    if(!game_id || !data.kits_type_name) throw new BadRequestException("Isi Data Dengan Benar")
+    return this.kitsTypeGameService.createKits({...data, game_id: Number(game_id) })
+  }
 
+  @Put('/:id/game/:game_id')
+  updateKits(
+    @Param('id', ValidateKitsTypeExist) id: string,
+    @Param('game_id', ValidateGameExist) game_id: string,
+    @Body() data: { kits_type_name: string }
+  ){
+    return this.kitsTypeGameService.updateKits(Number(id), {...data, game_id: Number(game_id)})
   }
 
   @Delete('/:id')
